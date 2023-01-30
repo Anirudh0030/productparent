5b31112fc3b25f98bf48f799dd50e86db05cdd0e
Anirudh0030 <chaubeanirudh@gmail.com>
2023-01-30 08:55:43 +0530

    Final changes

--- productservices/src/test/java/com/bharath/product/dao/ProductDAOImplTest.java
+++ productservices/src/test/java/com/bharath/product/dao/ProductDAOImplTest.java
@@ -21,7 +21,7 @@ public class ProductDAOImplTest {
 		Product result = dao.read(1);
 
 		assertNotNull(result);
-		assertEquals("Ipad", result.getName());
+		assertEquals("IPhone", result.getName());
 
 	}
 
